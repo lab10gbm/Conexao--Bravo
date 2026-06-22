5476f46e689c5619a02d1fc79aeffa759aae7ad4
lab10gbm <lab.10gbm@gmail.com>
2026-06-22 19:16:44 -0300

    feat: Integração Nativa Android, Capacitor e Capgo OTA (Permissões de Brado)

--- android/test-esbuild.ts
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';
-let req;
-if (typeof require !== 'undefined') {
-  req = require;
-} else {
-  // @ts-ignore
-  req = createRequire(import.meta.url);
-}
-console.log(typeof req);
